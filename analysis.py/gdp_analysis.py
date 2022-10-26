dc4e23266268193a2f82f1be8682dbabcec62ef3
TechMikeSol <cortezmichael786@yahoo.com>
2022-10-25 21:30:57 -0400

    removed to do

--- analysis.py/gdp_analysis.py
+++ analysis.py/gdp_analysis.py
@@ -1,14 +1,25 @@
 import csv
 
 def get_highest_gdp(data, year):
-    pass
+    maximum = float(data[0][year])
+    for row in data:
+        value = float(row[year])
+        if value > maximum:
+            maximum = value
+    return maximum
+    
 
 def get_lowest_gdp(data, year):
-    pass
+    minimum = float(data[0][year])
+    for row in data:
+        value = float(row[year])
+        if value < minimum:
+            minimum = value
+    return minimum
 
-# save each row into a list (TODO: change to your path!)
+# save each row into a list 
 list_data = []
-with open("data/state_gdp_analysis.csv", "r") as infile:
+with open("state_gdp_analysis.csv", "r") as infile:
     # load in data as DictReader
     reader = csv.DictReader(infile)
     # go through each year and get highest and lowest gdp
@@ -16,6 +27,7 @@ with open("data/state_gdp_analysis.csv", "r") as infile:
         list_data.append(row)
 
 # get highest gdp for 2020 using "get_highest_gdp(list_data, '2020')"
-
+print(reader.get_highest_gdp(""))
 
 # get lowest gdp for 2020 using "get_lowest_gdp(list_data, '2020')"
+
